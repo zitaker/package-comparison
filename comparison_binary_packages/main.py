3a3324e8fd0147bf8213fc3dd1441b3f20dde955
georg <georbearwolf.com>
2024-01-26 14:45:31 +0300

    it remains to conduct tests

--- comparison_binary_packages/main.py
+++ comparison_binary_packages/main.py
@@ -1,6 +1,7 @@
 import os
 import json
 
+
 from package_comparison import compare_packages
 from cli import parse_args
 from split_by_arch import split_by_arch
@@ -18,6 +19,17 @@ def save_data(data, name_file):
         json.dump(data, json_file, ensure_ascii=False, indent=4)
 
 
+def save_result(directory, data, name_file):
+    find_index = directory.rfind('/', 0, -3)
+    if find_index != -1:
+        path_for_save = directory[:find_index]
+
+    path = os.path.join(path_for_save, f"{name_file}.json")
+
+    with open(path, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=4)
+
+
 def split_by_directory(path_file):
     with open(path_file, 'r') as file:
         reading_data = json.load(file)
@@ -28,17 +40,22 @@ def split_by_directory(path_file):
         arch_values.add(arch_value)
 
 
-def main():
-    # print('wait - the download is in progress.')
-    # packages_p10, packages_sisyphus = parse_args()
-    # save_data(packages_p10, 'p10')
-    # print('The p10 packages are assembled.')
-    # save_data(packages_sisyphus, 'sisyphus')
-    # print('The sisyphus packages are assembled.')
-    # print('-----------------')
+def paths_to_files(file_paths):
+    path_1, path_2 = file_paths
+    return path_1, path_2
 
-    path_p10 = 'tests/fixtures/p101.json'
-    path_sisyphus = 'tests/fixtures/sisyphus1.json'
+
+def main():
+    print('wait - the download is in progress.')
+    packages_p10, packages_sisyphus = parse_args()
+    save_data(packages_p10, 'p10')
+    print('The p10 packages are assembled.')
+    save_data(packages_sisyphus, 'sisyphus')
+    print('The sisyphus packages are assembled.')
+    print('-----------------')
+
+    path_p10 = 'tests/fixtures/p10.json'
+    path_sisyphus = 'tests/fixtures/sisyphus.json'
 
     split_by_directory(path_p10)
     split_by_directory(path_sisyphus)
@@ -48,9 +65,17 @@ def main():
     split_by_arch(path_sisyphus)
     print('Packages by arch_value were extracted.')
 
-    print('----------')
-    save_result_packages()
-    print('----------')
+    list_path_files = save_result_packages()
+
+    for path in list_path_files:
+        result_pair = paths_to_files(path)
+        result = compare_packages(result_pair[0], result_pair[1])
+
+        save_result(result_pair[0], result[0], 'packages_p10_not_in_sisyphus')
+        save_result(result_pair[0], result[1], 'packages_sisyphus_not_in_p10')
+        save_result(result_pair[0], result[2], 'greater_version_release_sisyphus')
+    print('Done - see the result.')
+
 
     # result = compare_packages(path_p10, path_sisyphus)
     #
--- comparison_binary_packages/save_result_packages.py
+++ comparison_binary_packages/save_result_packages.py
@@ -12,11 +12,38 @@ def get_paths_directories_arch_value(path):
     list_paths = []
     for elem in list_directories:
         list_paths.append(os.path.join(path, elem))
-
     return list_paths
+
+
+def get_file_paths(directory):
+    file_paths = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_paths.append(os.path.join(root, file))
+    return file_paths
+
+def get_paths_of_list(directory_list):
+    pairs = []
+    for path in directory_list:
+        if os.path.isdir(path):
+            files = get_file_paths(path)
+            if len(files) == 1:
+                continue
+            elif len(files) >= 2:
+                pairs.append((files[0], files[1]))
+    return pairs
+
+def split_into_directory(list_paths):
+    result_pairs = []
+    for path in list_paths:
+        pairs = get_paths_of_list([path])
+        if pairs:
+            result_pairs.extend(pairs)
+    return result_pairs
 
 
 def save_result_packages():
     path = 'tests/fixtures/'
     list_path = get_paths_directories_arch_value(path)
-    print(list_path)
+    list_path_files = split_into_directory(list_path)
+    return list_path_files
