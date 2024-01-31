64e4cfcb6b657d9c6f9d42ea7e4868fe6af27a5e
georg <georbearwolf.com>
2024-01-31 16:37:05 +0300

    it remains to test on alt linux

--- comparison_binary_packages/package_comparison.py
+++ comparison_binary_packages/package_comparison.py
@@ -18,32 +18,46 @@ def packages(in_package, no_in_package):
     return result
 
 
+def additional_comparison(new_package, old_package, field):
+    if new_package[field] > old_package[field]:
+        return 1
+    elif new_package[field] < old_package[field]:
+        return -1
+    else:
+        return 0
+
+
 def greater_version_release(new_packages, old_packages):
     result = []
+    old_packages_index = {}
+
+    for old_package in old_packages:
+        old_packages_index[old_package["name"]] = old_package
 
     for new_package in new_packages:
         name = new_package["name"]
+        if name in old_packages_index:
+            old_package = old_packages_index[name]
 
-        for old_package in old_packages:
-            if old_package["name"] == name:
-                comparison = rpm.labelCompare(
-                    (str(new_package['epoch']),
-                     new_package['version'],
-                     new_package['release'],),
-                    (str(old_package['epoch']),
-                     old_package['version'],
-                     old_package['release']))
+            comparison = rpm.labelCompare(
+                (str(new_package['epoch']),
+                 new_package['version'],
+                 new_package['release'],),
+                (str(old_package['epoch']),
+                 old_package['version'],
+                 old_package['release']))
 
-                if comparison == 0:
-                    if new_package['disttag'] != old_package['disttag']:
-                        comparison = 1 if new_package['disttag'] > old_package['disttag'] else -1
-                    elif new_package['buildtime'] != old_package['buildtime']:
-                        comparison = 1 if new_package['buildtime'] > old_package['buildtime'] else -1
-                    elif new_package['source'] != old_package['source']:
-                        comparison = 1 if new_package['source'] > old_package['source'] else -1
+            if comparison == 0:
+                if new_package['disttag'] != old_package['disttag']:
+                    comparison = additional_comparison(new_package, old_package, 'disttag')
+                elif new_package['buildtime'] != old_package['buildtime']:
+                    comparison = additional_comparison(new_package, old_package, 'buildtime')
+                elif new_package['source'] != old_package['source']:
+                    comparison = additional_comparison(new_package, old_package, 'source')
 
-                if comparison > 0:
-                    result.append(new_package)
+            if comparison > 0:
+                result.append(new_package)
+
     return result
 
 
