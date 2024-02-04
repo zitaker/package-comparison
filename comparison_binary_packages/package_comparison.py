2c7a6365ffbfc4b132de02b3cf7e5cafe007a148
georg <georbearwolf.com>
2024-02-04 03:09:47 +0300

    checking in alt

--- comparison_binary_packages/package_comparison.py
+++ comparison_binary_packages/package_comparison.py
@@ -18,10 +18,10 @@ def packages(in_package, no_in_package):
     return result
 
 
-def additional_comparison(new_package, old_package, field):
-    if new_package[field] > old_package[field]:
+def additional_comparison(new_package, old_package, meaning):
+    if new_package[meaning] > old_package[meaning]:
         return 1
-    elif new_package[field] < old_package[field]:
+    elif new_package[meaning] < old_package[meaning]:
         return -1
     else:
         return 0
@@ -32,20 +32,24 @@ def greater_version_release(new_packages, old_packages):
     old_packages_index = {}
 
     for old_package in old_packages:
-        old_packages_index[old_package["name"]] = old_package
+        old_packages_index[old_package['name']] = old_package
 
     for new_package in new_packages:
-        name = new_package["name"]
+        name = new_package['name']
         if name in old_packages_index:
             old_package = old_packages_index[name]
 
-            comparison = rpm.labelCompare(
-                (str(new_package['epoch']),
-                 new_package['version'],
-                 new_package['release'],),
-                (str(old_package['epoch']),
-                 old_package['version'],
-                 old_package['release']))
+            package_1 = rpm.hdr()
+            package_2 = rpm.hdr()
+            package_1['epoch'] = new_package.get('epoch', 0)
+            package_1['version'] = new_package['version']
+            package_1['release'] = new_package['release']
+
+            package_2['epoch'] = old_package.get('epoch', 0)
+            package_2['version'] = old_package['version']
+            package_2['release'] = old_package['release']
+
+            comparison = rpm.versionCompare(package_1, package_2)
 
             if comparison == 0:
                 if new_package['disttag'] != old_package['disttag']:
@@ -67,7 +71,7 @@ def compare_packages(path_p10, path_sisyphus):
 
     packages_p10_not_in_sisyphus = packages(packages_p10, packages_sisyphus)
     packages_sisyphus_not_in_p10 = packages(packages_sisyphus, packages_p10)
-    greater_version_release_sisyphus = greater_version_release(packages_sisyphus, packages_p10)
+    greater_version_release_sisyphus = greater_version_release(packages_p10, packages_sisyphus)
 
     return (packages_p10_not_in_sisyphus,
             packages_sisyphus_not_in_p10,
